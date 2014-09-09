370c59f7c0dde96ba1445ad0fad6a6f258b25d34
leedam01 <leedam01@students.zhaw.ch>
2014-09-09 20:18:33 +0200

    Update boot.sh

--- _students/DamHoonLee/v01/boot.sh
+++ _students/DamHoonLee/v01/boot.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get install apache2
-apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
-apt-get install php5 libapache2-mod-php5 php5-mcrypt
+apt-get install -y apache2
+apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
+apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
